eac33889683b45624ea92cf9c88e2b21b5909b60
Sammybett-bit <bettsammy20@gmail.com>
2023-08-02 10:28:25 +0300

    called the functions  to display the actions

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function collectFood() {}
 function distributeFood() {
     const food = prompt('Enter food name');
     const placeDistributed =prompt('Enter the place the food to be distributed')
+    distributeFood.push({food, placeDistributed });
+    displayStatus("Added food name: " + food + " (place Distributed:" + placeDistributed + ")");
+
 }
 
 function addVolunteer() {
 const name = prompt("Enter volunteer's name:");
 const role = prompt("Enter volunteer's role:");
+volunteers.push({ name, role });
+displayStatus("Added volunteer: " + name + " (Role: " + role + ")");
 }
